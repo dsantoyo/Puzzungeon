556b1060c0682e4c0eb9b537727a25494fbca7b8
Yi Sui <ysui@usc.edu>
2018-11-03 12:25:19 -0700

    init commit

---
+++ desktop/src/project/puzzungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package project.puzzungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import project.puzzungeon.Puzzungeon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Puzzungeon(), config);
+	}
+}
