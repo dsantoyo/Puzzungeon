01ba9f99a6bc619cd4823da35475e1085186addd
Yi Sui <ysui@usc.edu>
2018-11-04 01:02:33 -0700

    changed Player.java

---
+++ core/src/project/puzzungeon/Player.java
@@ -0,0 +1,15 @@
+package project.puzzungeon;
+
+public class Player {
+	
+	Player(String username){
+		this.username = username;
+	}
+	
+	String username;
+	
+	//Avatar avatar ?
+	//Score score ?
+	
+	//other in-game objects that the server should know...
+}
