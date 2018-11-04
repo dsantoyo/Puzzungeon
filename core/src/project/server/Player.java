73ed6be78d5bbf2a7c37516ae1ada8ed9d8c75b2
Yi Sui <ysui@usc.edu>
2018-11-04 15:50:29 -0800

    updated Player.java

---
+++ core/src/project/server/Player.java
@@ -0,0 +1,31 @@
+package project.server;
+
+import java.io.Serializable;
+
+/*
+ * 
+Each player object represents a player in the game
+Each client has two players: localPlayer / otherPlayer
+
+The server has a player vector/array to store the players
+Each client updates its localPlayer and send it to the server
+The server sends the information the other player back to each client
+
+*/
+
+public class Player implements Serializable{
+	public static final long serialVersionUID = 1;
+	public Player(String username){
+		this.username = username;
+		this.playerID = 0;
+	}
+	
+	public String username;
+	public int playerID;
+	
+	//Avatar avatar ?
+	//Score score ?
+	
+	//other in-game objects that the server should know...
+}
+
