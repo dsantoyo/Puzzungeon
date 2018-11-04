60a0a6d1e8c58c2f1e18eeb30e05bd7537c70c58
Yi Sui <ysui@usc.edu>
2018-11-03 20:16:52 -0700

    "fixed a bug caused by the merge"

--- core/src/project/puzzungeon/screens/LoginScreen.java
+++ core/src/project/puzzungeon/screens/LoginScreen.java
@@ -80,8 +80,7 @@ public class LoginScreen implements Screen{
 					guestButton.addListener(new ClickListener() {
 						@Override
 						public void clicked(InputEvent event, float x, float y) {
-							game.client.client
-                = "Guest";
+							game.client.clientUsername = "Guest";
 							System.out.println("Trying to connect...");
 							game.client.connect();
 							System.out.println("connected!");
