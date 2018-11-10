94b22ef2857a5528c18e3329982a0c4f418342cf
Yi Sui <ysui@usc.edu>
2018-11-09 22:00:00 -0800

    working on database

--- core/src/project/server/game_database.sql
+++ core/src/project/server/game_database.sql
@@ -1,27 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
---
--- Host: localhost    Database: game_database
--- ------------------------------------------------------
--- Server version	8.0.12
+DROP DATABASE IF EXISTS game_database;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE game_database;
 
---
--- Table structure for table `highscore_table`
---
+USE game_database;
 
-DROP TABLE IF EXISTS `highscore_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `highscore_table` (
   `user1` int(11) NOT NULL,
   `user2` int(11) NOT NULL,
@@ -30,49 +12,11 @@ CREATE TABLE `highscore_table` (
   KEY `user2` (`user2`),
   CONSTRAINT `highscore_table_ibfk_1` FOREIGN KEY (`user1`) REFERENCES `user_table` (`userID`),
   CONSTRAINT `highscore_table_ibfk_2` FOREIGN KEY (`user2`) REFERENCES `user_table` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ;
 
---
--- Dumping data for table `highscore_table`
---
-
-LOCK TABLES `highscore_table` WRITE;
-/*!40000 ALTER TABLE `highscore_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `highscore_table` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_table`
---
-
-DROP TABLE IF EXISTS `user_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `user_table` (
   `userID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `userpassword` varchar(45) NOT NULL,
   PRIMARY KEY (`userID`,`username`,`userpassword`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_table`
---
-
-LOCK TABLES `user_table` WRITE;
-/*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-10-22 10:01:13
+);
