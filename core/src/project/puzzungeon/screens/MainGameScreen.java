748291c4f75ce80fdb67bef208f99a0c4f3f68f8
Yi Sui <ysui@usc.edu>
2018-11-11 00:11:12 -0800

    testing drag and drop

--- core/src/project/puzzungeon/screens/MainGameScreen.java
+++ core/src/project/puzzungeon/screens/MainGameScreen.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
@@ -60,7 +62,7 @@ public class MainGameScreen implements Screen{
 
 	@Override
 	public void show() {
-		
+				
 /****************************************************************************************
 *                             start: actors functionality
 ****************************************************************************************/
--- core/src/project/puzzungeon/screens/MainMenuScreen.java
+++ core/src/project/puzzungeon/screens/MainMenuScreen.java
@@ -4,14 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Target;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import project.puzzungeon.Client;
@@ -33,6 +40,13 @@ public class MainMenuScreen implements Screen{
 	private Dialog gameFullDialog;
 	private Dialog connectionFailDialog;
 	
+	
+	public Image testImage1;
+	public Image testImage2;
+	public Image table1Image;
+	public Image table2Image;
+	public Image table3Image;
+	
 	//constructor
 	public MainMenuScreen(Puzzungeon game) {
 		this.game = game;
@@ -48,6 +62,140 @@ public class MainMenuScreen implements Screen{
 /****************************************************************************************
 *                             start: actors functionality
 ****************************************************************************************/
+		
+		
+		testImage1 = new Image(new Texture(Gdx.files.internal("badlogic.jpg")));
+		testImage2 = new Image(new Texture(Gdx.files.internal("green.png")));
+		table1Image = new Image(new Texture(Gdx.files.internal("black.png")));
+		table2Image = new Image(new Texture(Gdx.files.internal("black.png")));
+		table3Image = new Image(new Texture(Gdx.files.internal("black.png")));
+		
+		final Table table1 = new Table().bottom();
+		table1.setFillParent(true);
+		table1.add(testImage1).width(100).height(100);
+		
+		final Table table2  = new Table().center();
+		table2.setFillParent(true);
+		table2.add(testImage2).width(100).height(100);
+		
+		final Table table3  = new Table().top();
+		table3.setFillParent(true);
+		table3.add(table3Image).width(100).height(100);
+				
+		
+		DragAndDrop dragAndDrop = new DragAndDrop();
+		
+		
+		dragAndDrop.addSource(new Source(table1) {
+			public Payload dragStart (InputEvent event, float x, float y, int pointer) {
+				
+				System.out.println("dragging from table1");
+				Payload payload = new Payload();
+				
+				payload.setObject(table1.getCells().get(0).getActor());
+
+				payload.setDragActor(table1.getCells().get(0).getActor());
+				table1.clearChildren();
+				table1.add(new Image(new Texture(Gdx.files.internal("black.png")))).width(100).height(100);
+				return payload;
+			}
+		});
+		
+		
+		dragAndDrop.addSource(new Source(table2) {
+			public Payload dragStart (InputEvent event, float x, float y, int pointer) {
+				
+				System.out.println("dragging from table2");
+				Payload payload = new Payload();
+				
+				payload.setObject(table2.getCells().get(0).getActor());
+
+				payload.setDragActor(table2.getCells().get(0).getActor());
+				table2.clearChildren();
+				table2.add(new Image(new Texture(Gdx.files.internal("black.png")))).width(100).height(100);
+				return payload;
+			}
+		});
+		
+		dragAndDrop.addSource(new Source(table3) {
+			public Payload dragStart (InputEvent event, float x, float y, int pointer) {
+				
+				System.out.println("dragging from table3");
+				Payload payload = new Payload();
+				
+				payload.setObject(table3.getCells().get(0).getActor());
+				payload.setDragActor(table3.getCells().get(0).getActor());
+				table3.clearChildren();
+				table3.add(new Image(new Texture(Gdx.files.internal("black.png")))).width(100).height(100);
+				return payload;
+			}
+		});
+		
+		
+		dragAndDrop.addTarget(new Target(table1) {
+			public boolean drag (Source source, Payload payload, float x, float y, int pointer) {
+				getActor().setColor(Color.GREEN);
+				return true;
+			}
+
+			public void reset (Source source, Payload payload) {
+				getActor().setColor(Color.WHITE);
+			}
+
+			public void drop (Source source, Payload payload, float x, float y, int pointer) {
+				System.out.println("table1 Accepted: " + payload.getObject() + " " + x + ", " + y);
+				((Table) getActor()).clearChildren();
+				((Table) getActor()).add((Image)payload.getObject()).width(100).height(100);
+			}
+		});
+		
+		
+		dragAndDrop.addTarget(new Target(table2) {
+			public boolean drag (Source source, Payload payload, float x, float y, int pointer) {
+				getActor().setColor(Color.GREEN);
+				return true;
+			}
+
+			public void reset (Source source, Payload payload) {
+				getActor().setColor(Color.WHITE);
+			}
+
+			public void drop (Source source, Payload payload, float x, float y, int pointer) {
+				System.out.println("table2 Accepted: " + payload.getObject() + " " + x + ", " + y);
+				((Table) getActor()).clearChildren();
+				((Table) getActor()).add((Image)payload.getObject()).width(100).height(100);
+			}
+		});
+		
+		
+		dragAndDrop.addTarget(new Target(table3) {
+			public boolean drag (Source source, Payload payload, float x, float y, int pointer) {
+				getActor().setColor(Color.GREEN);
+				return true;
+			}
+
+			public void reset (Source source, Payload payload) {
+				getActor().setColor(Color.WHITE);
+			}
+
+			public void drop (Source source, Payload payload, float x, float y, int pointer) {
+				System.out.println("table3 Accepted: " + payload.getObject() + " " + x + ", " + y);
+				((Table) getActor()).clearChildren();
+				((Table) getActor()).add((Image)payload.getObject()).width(100).height(100);
+			}
+		});
+		
+		
+		
+		stage.addActor(table1);
+		stage.addActor(table2);
+		stage.addActor(table3);
+		
+
+		
+		
+		
+///////////////////////////
 		Label gameTitle = new Label("Puzzungeon", game.skin);
 		
 		TextButton loginButton = new TextButton("Login", game.skin, "default");
@@ -159,8 +307,8 @@ public class MainMenuScreen implements Screen{
 		
 		
 		//add actors onto the stage
-		stage.addActor(mainMenuTable);
-		stage.addActor(exitButtonTable);
+		//stage.addActor(mainMenuTable);
+		//stage.addActor(exitButtonTable);
 		
 		//draw debugline to see the boundary of each actor
 		if(game.showDebugLine) {
