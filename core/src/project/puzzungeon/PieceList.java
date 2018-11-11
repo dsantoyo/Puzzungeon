ec0047806e40952747cd2ce4b2a2f638e1dadde2
Yi Sui <ysui@usc.edu>
2018-11-11 00:11:31 -0800

    add testing images

---
+++ core/src/project/puzzungeon/PieceList.java
@@ -0,0 +1,13 @@
+package project.puzzungeon;
+
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class PieceList<T> extends List<T>{
+
+	PieceList(Skin skin){
+		super(skin);
+	}
+	
+
+}
