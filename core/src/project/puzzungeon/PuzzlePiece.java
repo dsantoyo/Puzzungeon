470a5b5e577dfa664a809fbfcf08aee6629aaa9e
Yi Sui <ysui@usc.edu>
2018-11-19 21:43:12 -0800

    added names/usc emails and ids to each java  and sql files

--- core/src/project/puzzungeon/PuzzlePiece.java
+++ core/src/project/puzzungeon/PuzzlePiece.java
@@ -1,3 +1,15 @@
+/*CSCI201 Final Project
+
+Project Name: Puzzungeon
+Project Number: 7
+Project Category: Game
+
+Daniel Santoyo: dsantoyo@usc.edu USC ID: 6926712177
+Hayley Pike: hpike@usc.edu USC ID: 8568149839
+Yi(Ian) Sui: ysui@usc.edu USC ID: 2961712187
+Ekta Gogri: egogri@usc.edu USC ID: 9607321862
+*/
+
 package project.puzzungeon;
 
 import com.badlogic.gdx.graphics.Texture;
